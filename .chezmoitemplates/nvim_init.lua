d61b1692b6aa541b1ad08ad5e72e86332b9ee551
Aditya <contact@adityapadala.com>
2025-06-24 20:53:13 +0000

    shit moves faster than llvm

--- .chezmoitemplates/nvim_init.lua
+++ .chezmoitemplates/nvim_init.lua
@@ -99,9 +99,6 @@ require("lazy").setup({
     opts_extend = { "sources.default" }
   },
 
-  -- For Copilot, maintain it but as a dependency of blink.cmp
-  { 'github/copilot.vim', },
-
   -- Fucking LSP garbage
   "mason-org/mason.nvim",
   "mason-org/mason-lspconfig.nvim",
@@ -162,59 +159,60 @@ require("lazy").setup({
     'tjdevries/present.nvim'
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "copilot",
-      vendors = {
-        ["openrouter"] = {
-          __inherited_from = 'openai',
-          endpoint = 'https://openrouter.ai/api/v1',
-          api_key_name = 'OPENROUTER_API_KEY',
-          model = 'anthropic/claude-3.5-sonnet',
-          -- model = 'google/gemini-2.0-flash-001',
-        },
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    provider = "copilot",
+    providers = {
+      ["openrouter"] = {
+        __inherited_from = 'openai',
+        endpoint = 'https://openrouter.ai/api/v1',
+        api_key_name = 'OPENROUTER_API_KEY',
+        model = 'anthropic/claude-3.5-sonnet',
+        -- model = 'google/gemini-2.0-flash-001',
       },
     },
   },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua",      -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+},
+   
   -- almighty osc copy
   {
     "ojroques/nvim-osc52",
@@ -234,6 +232,8 @@ require("lazy").setup({
     end,
   }
 })
+
+require("copilot").setup()
 
 require('nvim-treesitter.install').compilers = { 'clang' }
 
